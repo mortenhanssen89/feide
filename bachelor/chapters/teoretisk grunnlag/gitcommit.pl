ec725a09e69cd5e4cc9dd8dc03f0d44c4d5ab6ce
root <root@m1330.(none)>
2013-04-17 12:13:23 +0200

    hs

---
+++ bachelor/chapters/teoretisk grunnlag/gitcommit.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+my $name;
+my $commit;
+
+print"file to commit:";
+$name = <>;
+system("git add $name");
+
+print"commit message:";
+$commit = <>;
+system("git commit -m '$commit'");
+
+#system("git remote add origin https://github.com/mortenhanssen89/feide.git");
+system("git pull origin master");
+system("git fetch origin master");
+system("git merge origin master");
+system("git push origin master");
